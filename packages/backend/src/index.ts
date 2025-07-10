c5eb4856ab0daafd445671f6166735862ad74d87
james-eo <jamesejike04@gmail.com>
2025-07-10 10:13:46 +0100

    feat: Added comunitiest routes

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,10 +1,11 @@
-import express, {Request, Response} from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import userRoutes from "./routes/user.routes";
-import authRoutes from "./routes/auth.routes"
-import swaggerUi from "swagger-ui-express";
-import {swaggerSpec} from "./swagger/swagger";
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.routes';
+import authRoutes from './routes/auth.routes';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerSpec } from './swagger/swagger';
+import communityRoutes from './routes/community.routes';
 
 dotenv.config();
 
@@ -14,18 +15,19 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 app.get('/health', (req: Request, res: Response) => {
   res.status(200).send('Server is healthy');
 });
 
-
 // Routes
 app.use('/api/auth', authRoutes);
-app.use("/api/users", userRoutes);
+app.use('/api/users', userRoutes);
+
+// community routes
+app.use('/api/communities', communityRoutes);
 
 // Swagger docs
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
