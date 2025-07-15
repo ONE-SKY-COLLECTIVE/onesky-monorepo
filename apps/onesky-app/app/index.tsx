25e848feef1211c643a4d0507d6b8d83c3b3d7c5
Stephen Shin <stephenshin@Stephens-Laptop.local>
2025-07-15 14:51:48 -0700

    fixing onboarding logic, quiz bug, adding redux structure and packages

--- apps/onesky-app/app/index.tsx
+++ apps/onesky-app/app/index.tsx
@@ -1,39 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { Redirect } from 'expo-router';
+import * as SecureStore from 'expo-secure-store';
 import Onboarding from './(onboarding)/index';
 import { SplashScreen } from '@/components/SplashScreen';
 
 export default function Index() {
   const [isSplashVisible, setIsSplashVisible] = useState(true);
   const [hasLaunched, setHasLaunched] = useState<boolean | null>(null);
+
   useEffect(() => {
     const initialize = async () => {
-      // Include this line of code to see how a first-time user will see the onboarding
-      // await SecureStore.deleteItemAsync("hasLaunched");
-      await new Promise(res => setTimeout(res, 8000));
+      await new Promise(res => setTimeout(res, 3000));
 
-      // const launchStatus = await SecureStore.getItemAsync("hasLaunched");
-      // if (launchStatus === null) {
-      //   await SecureStore.setItemAsync("hasLaunched", "true");
-      //   setHasLaunched(true);
-      // } else {
-      //   setHasLaunched(false);
-      // }
-      // await SecureStore.deleteItemAsync("hasLaunched");
-      setHasLaunched(true);
+      const launchStatus = await SecureStore.getItemAsync("hasLaunched");
+
+      if (launchStatus === null) {
+        await SecureStore.setItemAsync("hasLaunched", "true");
+        setHasLaunched(true);
+      } else {
+        setHasLaunched(false);
+      }
+
       setIsSplashVisible(false);
     };
+
     initialize();
   }, []);
 
   if (isSplashVisible || hasLaunched === null) {
     return <SplashScreen />;
   }
-  return hasLaunched ? <Onboarding /> : <Redirect href={'/(tabs)/(home)'} />;
-  // return (
-  //   <>
-  //     <SplashScreen />
-  //     <Redirect href={'/(tabs)'} />
-  //   </>
-  // );
+
+  return hasLaunched
+    ? <Onboarding />
+    : <Redirect href={'/(tabs)/(home)'} />;
 }
